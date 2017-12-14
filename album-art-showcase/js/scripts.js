b7cdf1457101799c39df10154a0bf6578d8f2884
Colin Worf and Lena Ha <colin@>
2017-12-14 11:45:26 -0800

    add style to nav and header

--- album-art-showcase/js/scripts.js
+++ album-art-showcase/js/scripts.js
@@ -1,7 +1,14 @@
 $(function() {
   // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
   $('#modal-form').modal();
-
+  $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year,
+      today: 'Today',
+      clear: 'Clear',
+      close: 'Ok',
+      closeOnSelect: false // Close upon selecting a date,
+    });
 // form here
 
 
